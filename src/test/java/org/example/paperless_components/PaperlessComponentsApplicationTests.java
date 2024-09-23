37880cb843bdddb54ac6bc6739024bfc12305c1d
eierschaedl <if21b172@technikum-wien.at>
2024-09-23 20:01:18 +0200

    1.0.0 init

---
+++ src/test/java/org/example/paperless_components/PaperlessComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paperless_components;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessComponentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
