2a9b00ee5c07aa4e7f1bb979eb98a719010822cf
Johannes Eiboeck <96199305+JhnnsEbck@users.noreply.github.com>
2024-11-11 22:24:02 +0100

    RabbitMQService + sendMessageToQueue beim /upload

--- src/main/java/org/example/paperless_components/RestAPI/api/DocumentAPI.java
+++ src/main/java/org/example/paperless_components/RestAPI/api/DocumentAPI.java
@@ -1,11 +1,13 @@
 package org.example.paperless_components.RestAPI.api;
 import org.example.paperless_components.RestAPI.service.mapper.DocumentService;
 import org.example.paperless_components.RestAPI.service.dtos.DocumentDto;
+import org.example.paperless_components.RestAPI.service.rabbitmq.RabbitMQService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,14 +22,17 @@ public class DocumentAPI {
     @PostMapping("/upload")
     public ResponseEntity<DocumentDto> postDocument(@RequestBody String document) {
         DocumentDto uploadedDocument = documentService.uploadDocument(document);
+        try {
+            new RabbitMQService().sendMessageToQueue("Document uploaded");
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
         return ResponseEntity.ok(uploadedDocument);
     }
 
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public ResponseEntity<List<DocumentDto>> getDocumentByText(@RequestParam String text) {
-        return null;
-    }
+    public ResponseEntity<List<DocumentDto>> getDocumentByText(@RequestParam String text) { return null; }
 
     @GetMapping("/{documentId}/metadata")
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
--- src/main/java/org/example/paperless_components/RestAPI/service/rabbitmq/RabbitMQService.java
+++ src/main/java/org/example/paperless_components/RestAPI/service/rabbitmq/RabbitMQService.java
@@ -1,35 +1,27 @@
 package org.example.paperless_components.RestAPI.service.rabbitmq;
 
+import com.rabbitmq.client.DeliverCallback;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import org.example.paperless_components.RestAPI.configuration.RabbitMQConfig;
 
-@Component
 @Slf4j
 public class RabbitMQService {
-    private final RabbitTemplate rabbit;
 
-    @Autowired
-    public RabbitMQService(RabbitTemplate rabbit) {
-        this.rabbit = rabbit;
-    }
-
-    @RabbitListener(queues = RabbitMQConfig.ECHO_IN_QUEUE_NAME)
-    public void processMessage(String message, @Header(RabbitMQConfig.ECHO_MESSAGE_COUNT_PROPERTY_NAME) int messageCount) {
-        log.info("Recieved Message #" + messageCount+ ": " + message);
-
-        // Delay
-        try {
-            Thread.sleep(message.length() * 1000L);
-        } catch (InterruptedException e) {
-            System.out.println("Delete2ndChar service interrupted");
-        }
-
-        rabbit.convertAndSend(RabbitMQConfig.ECHO_OUT_QUEUE_NAME, "Echo " + message);
-        log.info("Sent Message: Echo " + message);
+    private RabbitMQConfig config = new RabbitMQConfig();
+    RabbitTemplate rabbitTemplate = config.rabbitTemplate();
+    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+        log.info("\nYou received: " + message);
+    };
+    public void sendMessageToQueue(String message) throws IOException {
+        config.connectionFactory()
+                .createConnection()
+                .createChannel(false)
+                .basicConsume(RabbitMQConfig.ECHO_OUT_QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+        log.info("You entered: " + message);
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, RabbitMQConfig.ECHO_IN_QUEUE_NAME, message);
     }
 }
