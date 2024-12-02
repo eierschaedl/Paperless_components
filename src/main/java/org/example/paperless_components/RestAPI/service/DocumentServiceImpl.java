c6b9a259bfd1ef27c042b5d1cf9f72830536b965
johannes <if21b048@technikum-wien.at>
2024-12-02 13:41:02 +0100

    Dockerzeug-2
    - Corrected Patherror

--- src/main/java/org/example/paperless_components/RestAPI/service/DocumentServiceImpl.java
+++ src/main/java/org/example/paperless_components/RestAPI/service/DocumentServiceImpl.java
@@ -5,7 +5,10 @@ import org.example.paperless_components.Persistance.entities.DocumentEntity;
 import org.example.paperless_components.Persistance.repos.DocumentRepo;
 import org.example.paperless_components.RestAPI.service.dtos.DocumentDto;
 import org.example.paperless_components.RestAPI.service.mapper.DocumentMapper;
+import org.example.paperless_components.RestAPI.service.rabbitmq.RabbitMQService;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
@@ -13,10 +16,12 @@ public class DocumentServiceImpl {
 
     private final DocumentMapper documentMapper;
     private final DocumentRepo documentRepository;
+    private final RabbitMQService rabbitMQService;
 
-    public DocumentServiceImpl(DocumentMapper documentMapper, DocumentRepo documentRepository) {
+    public DocumentServiceImpl(DocumentMapper documentMapper, DocumentRepo documentRepository, RabbitMQService rabbitMQService) {
         this.documentMapper = documentMapper;
         this.documentRepository = documentRepository;
+        this.rabbitMQService = rabbitMQService;
     }
 
     public DocumentDto uploadDocument(DocumentDto documentDto) {
@@ -25,6 +30,8 @@ public class DocumentServiceImpl {
 
         log.info("createdDocument: {}", savedEntity);
 
+        // Send message to RabbitMQ
+        rabbitMQService.sendMessageToQueue("Document uploaded: " + savedEntity.getId());
         return documentMapper.mapToDto(savedEntity);
     }
 }
