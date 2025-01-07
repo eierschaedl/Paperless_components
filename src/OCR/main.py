80a12060148956dbb9ff4d1b479d233810137349
Johannes Eiboeck <96199305+JhnnsEbck@users.noreply.github.com>
2025-01-07 03:32:46 +0100

    Merge remote-tracking branch 'origin/Sprint-5' into Sprint-5

--- src/OCR/main.py
+++ src/OCR/main.py
@@ -109,7 +109,7 @@ def main():
     channel.basic_consume(
         queue='OCR_QUEUE',
         on_message_callback=callback,
-        auto_ack=True
+        auto_ack=False
     )
 
     print("[*] Waiting for messages. To exit press CTRL+C")
